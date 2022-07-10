466fa4417c5c15d6dad10928db076aa3393c69d1
branyerbjr <branyer.romero@tecsup.edu.pe>
2022-07-10 18:15:03 -0500

    plantilla en avanze

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,11 @@
 //iniciando comentario javaScritp
 
-console.log("hola")
+console.log("BIENVENIDO A REABBIT\n 2022")
+
+const allItems = document.querySelectorAll('.prueba ul li a');
+
+allItems.forEach(item => {
+    item.addEventListener("click", function(e){
+        this.classList.add("active");
+    });
+});
