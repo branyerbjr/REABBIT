026d9e1f05c2c1614545cd528bd8d6aab28cda42
branyerbjr <branyer.romero@tecsup.edu.pe>
2022-07-18 08:38:46 -0500

    anotacione commit

--- scripts/registro.js
+++ scripts/registro.js
@@ -1,28 +1,28 @@
-const prevBtns = document.querySelectorAll(".btn-prev");
-const nextBtns = document.querySelectorAll(".btn-next");
+const prevBtns = document.querySelectorAll(".btn-prev"); // el metodo selecciona el elemento.
+const nextBtns = document.querySelectorAll(".btn-next"); // btn-prev - btn-next son tipo clase de selector.
 const progress = document.getElementById("progress");
 const formSteps = document.querySelectorAll(".form-step");
 const progressSteps = document.querySelectorAll(".progress-step");
 
 let formStepsNum = 0;
 
-nextBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
+nextBtns.forEach((btn) => { //El metodo  se comporta como un bucle asi nuestro evento click
+  btn.addEventListener("click", () => {//ese evento pertence al boton siguiente
     formStepsNum++;
     updateFormSteps();
     updateProgressbar();
   });
 });
 
-prevBtns.forEach((btn) => {
-  btn.addEventListener("click", () => {
+prevBtns.forEach((btn) => { // El metodo  se comporta como un bucle asi nuestro evento click
+  btn.addEventListener("click", () => { //ese evento pertence al boton retroceder
     formStepsNum--;
     updateFormSteps();
     updateProgressbar();
   });
 });
 
-function updateFormSteps() {
+function updateFormSteps() {  //activamos activar la linea en  progreso
   formSteps.forEach((formStep) => {
     formStep.classList.contains("form-step-active") &&
       formStep.classList.remove("form-step-active");
@@ -31,7 +31,7 @@ function updateFormSteps() {
   formSteps[formStepsNum].classList.add("form-step-active");
 }
 
-function updateProgressbar() {
+function updateProgressbar() { //progreso de del pregress bar
   progressSteps.forEach((progressStep, idx) => {
     if (idx < formStepsNum + 1) {
       progressStep.classList.add("progress-step-active");
