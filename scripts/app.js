df55e8408ce6a90c0c6f0c2e7c778647bbbc4910
branyerbjr <branyer.romero@tecsup.edu.pe>
2022-07-17 19:35:57 -0500

    borrar

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,3 @@
 //iniciando comentario javaScritp
 
 console.log("BIENVENIDO A REABBIT\n 2022")
-
-const allItems = document.querySelectorAll('.prueba ul li a');
-
-allItems.forEach(item => {
-    item.addEventListener("click", function(e){
-        this.classList.add("active");
-    });
-});
